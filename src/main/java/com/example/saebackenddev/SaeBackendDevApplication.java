a1cfc0d37a302bc79f45149ff58cdeb261775bf6
jwywoo <jwywoo26@gmail.com>
2025-02-18 18:01:07 +0900

    [FEAT] #1 Project Setting
    2트

---
+++ src/main/java/com/example/saebackenddev/SaeBackendDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.saebackenddev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaeBackendDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaeBackendDevApplication.class, args);
+    }
+
+}
